3ba63b3cc5d6653311c5a5b18d58f059de37ccc9
PacoRP1 <86936729+PacoRP1@users.noreply.github.com>
2021-08-05 20:42:11 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.JailBlip = vector3(1855.1,  2582.72, 45.67)
+Config.JailLocation = vector3(1642.92, 2528.84, 45.56)
+Config.JailTimeSyncInterval = 60000 * 1
+
+
+Config.Uniforms = {
+	prison_wear = {
+		male = {
+			['tshirt_1'] = 15,  ['tshirt_2'] = 0,
+			['torso_1']  = 5, ['torso_2']  = 0,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 5,   ['pants_1']  = 35,
+			['pants_2']  = 0,   ['shoes_1']  = 66,
+			['shoes_2']  = 0,  ['chain_1']  = 0,
+			['chain_2']  = 0
+		},
+		female = {
+			['tshirt_1'] = 3,   ['tshirt_2'] = 0,
+			['torso_1']  = 38,  ['torso_2']  = 3,
+			['decals_1'] = 0,   ['decals_2'] = 0,
+			['arms']     = 2,   ['pants_1']  = 3,
+			['pants_2']  = 15,  ['shoes_1']  = 66,
+			['shoes_2']  = 5,   ['chain_1']  = 0,
+			['chain_2']  = 2
+		}
+	}
+}
